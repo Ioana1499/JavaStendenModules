dc71dc677eb8962fb71a6d6440a0dfe45a7ed871
Joris Rietveld <jorisrietveld@gmail.com>
2017-10-16 14:33:18 +0200

    Switched to java FXML

--- Java2/src/EndAssignmend/Assignment6/Main/GUI/Controller.java
+++ Java2/src/EndAssignmend/Assignment6/Main/GUI/Controller.java
@@ -2,9 +2,9 @@ package Assignment6.Main.GUI;
 
 /**
  * Author: Joris Rietveld <jorisrietveld@gmail.com>
- * Created: 16-10-2017 13:58
+ * Created: 16-10-2017 14:29
  * License: GPLv3 - General Public License version 3
  */
-public class ContainerGui
+public class Controller
 {
 }
---
+++ Java2/src/EndAssignmend/Assignment6/Main/GUI/Main.java
@@ -0,0 +1,16 @@
+package Assignment6.Main.GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Author: Joris Rietveld <jorisrietveld@gmail.com>
+ * Created: 16-10-2017 14:29
+ * License: GPLv3 - General Public License version 3
+ */
+public class Main
+{
+}
